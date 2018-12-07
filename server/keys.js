cec09ac52525b4ed063894b5098431b0ec9fcb0c
Roberto fraga Piñeiro <info@roberdev.io>
2018-12-07 13:40:59 +0100

    First commit

--- server/keys.js
+++ server/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PGUSER,
   pgHost: process.env.PGHOST,
+  pgUser: process.env.PGUSER,
   pgDatabase: process.env.PGDATABASE,
   pgPassword: process.env.PGPASSWORD,
   pgPort: process.env.PGPORT
